ced98127b94741259363e139551cb1554cc52388
hoangnguyen2003 <ninjadoc0152@gmail.com>
2025-07-06 02:29:55 +0700

    minor changes

--- main.py
+++ main.py
@@ -6,31 +6,31 @@ from transformers import AutoTokenizer
 from sentence_transformers import SentenceTransformer
 
 def main():
-    topic = input("Enter a topic to learn about: ")
+    topic = input('Enter a topic to learn about: ')
     document = get_wikipedia_content(topic)
-    if not document:
-        print("Could not retrieve information.")
+    if document is None:
+        print('Could not retrieve information.')
         return
 
-    tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-mpnet-base-v2")
+    tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-mpnet-base-v2')
     chunks = split_text(document, tokenizer)
 
-    embedding_model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+    embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
     embeddings = get_embeddings(chunks, embedding_model)
 
     index = build_faiss_index(embeddings)
 
-    qa_pipeline = setup_qa_pipeline("deepset/roberta-base-squad2")
+    qa_pipeline = setup_qa_pipeline('deepset/roberta-base-squad2')
     
     while True:
-        query = input("\nAsk a question about the topic (or 'quit' to exit): ")
+        query = input('\nAsk a question about the topic (or "quit" to exit): ')
         if query.lower() == 'quit':
             break
             
         retrieved_chunks = retrieve_chunks(query, index, embedding_model, chunks)
-        context = " ".join(retrieved_chunks)
+        context = ' '.join(retrieved_chunks)
         answer = get_answer(qa_pipeline, query, context)
-        print(f"Answer: {answer['answer']}")
+        print(f'Answer: {answer['answer']}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
